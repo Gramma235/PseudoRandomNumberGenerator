13f16ccd21ca2ce600e3e5facdedec5684e1f56b
Gramma <wladaha12@gmail.com>
2021-02-28 13:55:12 +0300

    Initial commit

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ def PseudoRandomNumberGenerator(seed,NumberLength):
     while flag==True:
         bufseed=seed**2
         if NumberLength%2!=0 and len(str(bufseed))%2==0 or NumberLength%2==0 and len(str(bufseed))%2!=0:
-            bufseed=int(str(bufseed)+'0')
+            bufseed=int('0'+str(bufseed))
         CutLength=int(round((len(str(bufseed))-NumberLength)/2,1))
         seed=int(str(bufseed)[CutLength:len(str(bufseed))-CutLength])
         for i in range(len(NumberList)):
             if seed==NumberList[i]:
                 flag=False
         if flag==True:
+            print(seed)
             if seed==0:
-                print(seed)
                 flag=False
             else:
                 NumberList.append(seed)
-                print(seed)
 PseudoRandomNumberGenerator(int(input('Введите начальное число: ')),int(input('Введите наибольшую длину числа на выходе: ')))
